660ce402441680b719abab95ad41574554ca3018
Ramdan Bahamalah <ramdan.pkh2016@gmail.com>
2017-12-31 17:09:56 +0700

    Update safelinkv2.js

--- safelinkv2.js
+++ safelinkv2.js
@@ -21,10 +21,10 @@ if(a_to_vh==false)
 {var encryptedUrl=Base64.encode(a_to_vi);
 var x=Math.floor((Math.random()*4)+ 1);
 var xxx=null;
-if(x=="1"){xxx="tips-for-maintaining-healthy-body.html"}
-if(x=="2"){xxx="10-tips-for-start-small-business-startup.html"}
-if(x=="3"){xxx="advantages-of-being-home-business-owner.html"}
-if(x=="4"){xxx="practice-environmental-qwareness-to-our.html"}a_to_ve[i].href="http://hanyasave.blogspot.com/2017/12/"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
+if(x=="1"){xxx="the-way-to-start-business-at-home.html"}
+if(x=="2"){xxx="the-influence-of-us-retail-sales-data.html"}
+if(x=="3"){xxx="blue-phosphor-interior-lighting-design.html"}
+if(x=="4"){xxx="avoid-risk-of-chronic-disease-to.html"}a_to_ve[i].href="http://hanyasave.blogspot.com/2017/12/"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
 var a_to_vj=document.getElementById("anonyminized");var a_to_vk=document.getElementById("found_links");if(a_to_vj)
 {a_to_vj.innerHTML+=a_to_vb;}
 if(a_to_vk)
